7955148f6e5e5872f0875972100b5973b7d15fbd
sstvskids <staavstudios@gmail.com>
2025-05-11 09:51:03 +1000

    Update mathexample.lua

--- examples/lua/mathexample.lua
+++ examples/lua/mathexample.lua
@@ -1,4 +1,6 @@
 local java = {
-    js = loadstring(game:HttpGet(''))(),
-    example = loadstring(game:HttpGet(''))()
+    js = loadstring(game:HttpGet('https://raw.githubusercontent.com/sstvskids/JStoLuaU/refs/heads/main/javaconvert.lua'))(),
+    example = loadstring(game:HttpGet('https://raw.githubusercontent.com/sstvskids/JStoLuaU/refs/heads/main/examples/javascript/mathexample.js'))()
 }
+
+return java.js.conversion.convertToLuau(java.example)
